f8f4967926da3bb01c179d56ba5c5013aa956091
Laxman Giri <laxmanvatij@gmail.com>
2025-01-25 22:22:13 +0100

    Built SIgnup and customized types

--- client/src/auth/Singup.tsx
+++ client/src/auth/Singup.tsx
@@ -1,31 +1,44 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { SignupInputState, userSignupSchema } from "@/schema/userSchema";
 import { Label } from "@radix-ui/react-label";
 import { Separator } from "@radix-ui/react-separator";
 import { Loader2, LockKeyhole, Mail, PhoneOutgoing, User } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Link } from "react-router";
-type SignUpForm={
-    fullname:string,
-    email:string,
-    password:string,
-    contact:string
-}
+// type SignUpForm={
+//     fullname:string,
+//     email:string,
+//     password:string,
+//     contact:string
+// }
 
 
 const Signup = () => {
-    const[input,setInput]=useState<SignUpForm>({
+    const[input,setInput]=useState<SignupInputState>({
             fullname:"",
             email:"",
             password:"",
             contact:""
     })
+    const [error,setError]=useState<Partial<SignupInputState>>({})
+
+
+
     const changeInputHandler=(e:ChangeEvent<HTMLInputElement>)=>{
             const {name,value}=e.target
             setInput({...input,[name]:value})
     }
     const submitHandler=(e:FormEvent)=>{
         e.preventDefault()
+        //formvalidation
+        const result=userSignupSchema.safeParse(input)
+        if(!result.success){
+          const fieldErrors=result.error.formErrors.fieldErrors
+          setError(fieldErrors as Partial<SignupInputState>)
+          return
+        }
+        //after api call 
     }
   const loading = false;
  
@@ -48,6 +61,7 @@ const Signup = () => {
               className="pl-10 focus-visible:ring-1"
             />
             <User className="absolute inset-y-8 left-2 pointer-events-none text-gray-500" />
+            {error&& <span className="text-sm text-red-700">{error?.fullname}</span>}
           </div>
         </div>
         <div className="mb-4">
@@ -62,6 +76,7 @@ const Signup = () => {
               className="pl-10 focus-visible:ring-1"
             />
             <Mail className="absolute inset-y-8 left-2 pointer-events-none text-gray-500" />
+            {error&& <span className="text-sm text-red-700">{error?.email}</span>}
           </div>
         </div>
         <div className="mb-4">
@@ -76,6 +91,7 @@ const Signup = () => {
               className="pl-10 focus-visible:ring-1"
             />
             <LockKeyhole className="absolute inset-y-8 left-2 pointer-events-none text-gray-500" />
+            {error&& <span className="text-sm text-red-700">{error?.password}</span>}
           </div>
         </div>
         <div className="mb-4">
@@ -90,6 +106,7 @@ const Signup = () => {
               className="pl-10 focus-visible:ring-1"
             />
             <PhoneOutgoing className="absolute inset-y-8 left-2 pointer-events-none text-gray-500" />
+            {error&& <span className="text-sm text-red-700">{error?.contact}</span>}
           </div>
         </div>
         <div className="mb-10">
