d430dd6658b654852280342c2a1b9071f56454ce
Laxman Giri <laxmanvatij@gmail.com>
2025-01-25 22:19:09 +0100

    Added Router for HeroSection&Profile &ForgotPassword&Resetpassword&VerifyEmail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,25 @@ import "./App.css";
 import Login from "./auth/Login";
 import MainLayout from "./MainLayout";
 import Signup from "./auth/Singup";
+import ForgotPassword from "./auth/ForgotPassword";
+import ResetPassword from "./auth/ResetPassword";
+import VerifyEmail from "./auth/VerifyEmail";
+import HeroSection from "./components/HeroSection";
+import Profile from "./components/Profile";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
+      {
+        path: "/",
+        element: <HeroSection/>,
+      },
+      {
+        path: "/profile",
+        element: <Profile/>,
+      },
       {
         path: "/login",
         element: <Login />,
@@ -17,6 +30,18 @@ const appRouter = createBrowserRouter([
         path: "/signup",
         element: <Signup />,
       },
+      {
+        path:"/forgot-password",
+        element:<ForgotPassword/>
+      },
+      {
+        path:"/reset-password",
+        element:<ResetPassword/>
+      },
+      {
+        path:"/verify-code",
+        element:<VerifyEmail/>
+      }
     ],
   },
 ]);
