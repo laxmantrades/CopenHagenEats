dce43f2a21dc4d718051140c7254c60134147cca
Laxman Giri <laxmanvatij@gmail.com>
2025-01-25 22:19:45 +0100

    addedFooter&& added CSS tot the mainlayout

--- client/src/MainLayout.tsx
+++ client/src/MainLayout.tsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router"
+import { Outlet } from "react-router";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-const MainLayout=()=>{
-    return(
-        <div>
-            <Outlet/>
-        </div>
-    )
-}
-export default MainLayout
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen m-2 md:m-0">
+      <header>
+        <NavBar />
+      </header>
+      <div className="flex-1">
+        <Outlet/>
+      </div>
+
+      
+        <footer><Footer></Footer></footer>
+      
+    </div>
+  );
+};
+export default MainLayout;
