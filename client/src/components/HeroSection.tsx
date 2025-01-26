7fee2fa3d676b4baaeeb32beb5d27ed736465647
Laxman Giri <laxmanvatij@gmail.com>
2025-01-26 21:11:31 +0100

    Added Input Section

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -2,8 +2,14 @@ import { Search } from "lucide-react"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
 import hero from "../assets/hero_pizza.png"
+import { Link } from "react-router"
+import { useState } from "react"
 
 const HeroSection=()=>{
+    const[searchtext,setSearchText]=useState<string>("")
+    const onChangeHandler=(e:React.ChangeEvent<HTMLInputElement>)=>{
+            setSearchText(e.target.value)
+    }
     return(
         <div className="flex items-center justify-center mt-10">
             <div>
@@ -11,9 +17,9 @@ const HeroSection=()=>{
                 <h1 className="font-extrabold">&anywherer</h1>
                 <p className="mt-2">Hey! Our Delicios food is waiting for you, we are always near to you!</p>
                 <div className="relative mt-4 flex space-x-2">
-                    <Input placeholder="Search " className=" pl-10 shadow-xl"/>
+                    <Input value={searchtext} onChange={onChangeHandler} placeholder="Search " className=" pl-10 shadow-xl"/>
                     <Search className="absolute inset-1 text-gray-500"/>
-                    <Button>Search</Button>
+                    <Link to={`/search/${searchtext}`}><Button >Search</Button></Link>
 
                 </div>
                 <div>
