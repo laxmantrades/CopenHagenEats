ba1dd9a27a9295a413bd69421e45bef6ee43df07
Laxman Giri <laxmanvatij@gmail.com>
2025-01-29 18:25:11 +0100

    built menuschema for form validation

---
+++ client/src/schema/menuSchema.ts
@@ -0,0 +1,10 @@
+import {z} from "zod"
+
+export const menuSchema=z.object({
+    name:z.string().nonempty({message:"Name is required"}),
+    description:z.string().nonempty({message:"Description is required"}),
+    price:z.number().min(0,{message:"Price can not be negetive"}),
+    image:z.instanceof(File).optional().refine((file)=>file?.size !== 0, {message:"Image file is required"})
+})
+
+export type MenuFormType=z.infer<typeof menuSchema>
