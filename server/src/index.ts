90af99f15667de2f01b99d742cc00ba0aeca8d93
Laxman Giri <laxmanvatij@gmail.com>
2025-02-02 21:51:46 +0100

    added route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./db/connectDb";
+import userRouter from "./routes/user.route";
+import { errorHandler } from "./middlewares/errorHandler";
+
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT;
+
+app.use(express.json())
+app.use("/api/v1/user",userRouter )
+app.use(errorHandler)
 
 connectDb().then(() =>
   app.listen(PORT, () => {
-    console.log(`Server is running at port to  ${PORT}`);
+    console.log(`Server is running at port too  ${PORT}`);
   })
 );
