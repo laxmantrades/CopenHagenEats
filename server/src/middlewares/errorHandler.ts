ac2cdbd41370337d8ae1648079579ad0e819cf8c
Laxman Giri <laxmanvatij@gmail.com>
2025-02-03 20:46:04 +0100

    formatted documnet

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,9 +1,13 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
 
-
-export const errorHandler=(err:any,req:Request,res:Response,next:NextFunction)=>{
-    res.status(500).json({
-        success:false,
-        message:"Internal Server Error"
-    })
-}
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error" + err.message,
+  });
+};
