abc0a7b19d12c7b8cd77804ecf9c85f7f6b95f82
Laxman Giri <laxmanvatij@gmail.com>
2025-02-03 20:47:19 +0100

    cookie generate

--- server/src/utils/generateCookie.ts
+++ server/src/utils/generateCookie.ts
@@ -1,8 +1,10 @@
 import { Response } from "express"
-import { IUserDocument } from "../models/resturant.model"
+import { IUserDocument } from "../models/user.model"
 import jwt from "jsonwebtoken"
+import { Document } from "mongoose"
 
-export const generateCookie=(res:Response,user:IUserDocument)=>{
+export const generateCookie=(res:Response,user:IUserDocument&Document)=>{
     const token=jwt.sign({userId:user._id},process.env.SECRET_KEY!,{expiresIn:"1d"})
-    res.cookie("token",token,{httpOnly:true, sameSite:'strict', maxAge:24*60*60*1000})
+    res.cookie("token",token,{httpOnly:true, maxAge:24*60*60*1000})
+    //return token
 }
