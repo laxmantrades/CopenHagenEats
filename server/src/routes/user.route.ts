bee622826a4a8b1ffce3f33ae8e3155e41ea92d1
Laxman Giri <laxmanvatij@gmail.com>
2025-02-03 20:46:48 +0100

    added routes

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,18 @@
 import express from "express";
-import { login, signup } from "../controllers/user.controller";
+import { checkAuth, forgotPassword, login, logout, resetPassword, signup, updateProfile, verifyEmail } from "../controllers/user.controller";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 const userRouter = express.Router();
 
+
+userRouter.route("/check-auth").get(isAuthenticated,checkAuth)
 userRouter.route("/signup").post(signup)
 userRouter.route("/login").post(login);
+userRouter.route("/logout").post(logout)
+userRouter.route("/verify-email").post(verifyEmail)
+userRouter.route("/forgot-password").post(forgotPassword)
+userRouter.route("/reset-password/:token").post(resetPassword),
+userRouter.route("profile/update").patch(isAuthenticated,updateProfile)
 
 
 export default userRouter;
