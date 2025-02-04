7a3ec2cea1a2acafa9bba7bcac51d36f72ef10b9
Laxman Giri <laxmanvatij@gmail.com>
2025-02-04 20:29:31 +0100

    added bodyParse&&express.urlencoded&&corsOption&&cookie-parse

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import connectDb from "./db/connectDb";
 import userRouter from "./routes/user.route";
 import { errorHandler } from "./middlewares/errorHandler";
-
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import cors from "cors"
 
 dotenv.config();
 
@@ -11,9 +13,19 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(bodyParser.json({limit:"10mb"}))
+app.use(express.urlencoded({extended:true,limit:"10mb"}))
+
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/v1/user",userRouter )
 app.use(errorHandler)
+
+const corsOptions={
+  origin:process.env.FRONTEND_URL,
+  credentials:true
+}
+app.use(cors(corsOptions))
 
 connectDb().then(() =>
   app.listen(PORT, () => {
