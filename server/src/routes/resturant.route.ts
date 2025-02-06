f6355c6cdc7a5a4f3aaaa9ce4e28a2e5bf945628
Laxman Giri <laxmanvatij@gmail.com>
2025-02-06 20:19:24 +0100

    formattedthe document

--- server/src/routes/resturant.route.ts
+++ server/src/routes/resturant.route.ts
@@ -1,14 +1,30 @@
-import  express  from "express";
+import express from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
-import { createResturant, getResturant, getResturantOrder, getSingleRestaurant, searchResturant, updateOrderStatus, updateResturant } from "../controllers/resturant.controller";
-import upload from "../middlewares/multer"
-const resturantRouter=express.Router()
+import {
+  createResturant,
+  getResturant,
+  getResturantOrder,
+  getSingleRestaurant,
+  searchResturant,
+  updateOrderStatus,
+  updateResturant,
+} from "../controllers/resturant.controller";
+import upload from "../middlewares/multer";
+const resturantRouter = express.Router();
 
-resturantRouter.route("/").post(isAuthenticated,upload.single("imageFile"),createResturant)
-resturantRouter.route("/").get(isAuthenticated,getResturant)
-resturantRouter.route("/").put(isAuthenticated,upload.single("imageFile"),updateResturant)
-resturantRouter.route("/order").get(isAuthenticated,getResturantOrder)
-resturantRouter.route("/order/:orderId/status").put(isAuthenticated,upload.single("imageFile"),updateOrderStatus)
-resturantRouter.route("/search/:searchText").get(isAuthenticated,searchResturant)
-resturantRouter.route("/:id").get(isAuthenticated,getSingleRestaurant)
-export default resturantRouter
+resturantRouter
+  .route("/")
+  .post(isAuthenticated, upload.single("imageFile"), createResturant);
+resturantRouter.route("/").get(isAuthenticated, getResturant);
+resturantRouter
+  .route("/")
+  .put(isAuthenticated, upload.single("imageFile"), updateResturant);
+resturantRouter.route("/order").get(isAuthenticated, getResturantOrder);
+resturantRouter
+  .route("/order/:orderId/status")
+  .put(isAuthenticated, upload.single("imageFile"), updateOrderStatus);
+resturantRouter
+  .route("/search/:searchText")
+  .get(isAuthenticated, searchResturant);
+resturantRouter.route("/:id").get(isAuthenticated, getSingleRestaurant);
+export default resturantRouter;
