0c8b32e906c2e848dad5a2a5e6f97f3f317438bd
Laxman Giri <laxmanvatij@gmail.com>
2025-02-07 21:04:49 +0100

    fixed cors error / moved use cors at top

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import connectDb from "./db/connectDb";
 import userRouter from "./routes/user.route";
 import { errorHandler } from "./middlewares/errorHandler";
-import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
 import resturantRouter from "./routes/resturant.route";
 import menuRouter from "./routes/menu.route";
 import orderRoute from "./routes/order.route";
@@ -15,24 +15,25 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
 
-app.use(bodyParser.json({limit:"10mb"}))
-app.use(express.urlencoded({extended:true,limit:"10mb"}))
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/v1/user",userRouter )
-app.use("/api/v1/resturant",resturantRouter)
-app.use("api/v1/menu",menuRouter)
-app.use("api/v1/order",orderRoute)
-app.use(errorHandler)
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/resturant", resturantRouter);
+app.use("api/v1/menu", menuRouter);
+app.use("api/v1/order", orderRoute);
+app.use(errorHandler);
 
 
-const corsOptions={
-  origin:process.env.FRONTEND_URL,
-  credentials:true
-}
-app.use(cors(corsOptions))
+
 
 connectDb().then(() =>
   app.listen(PORT, () => {
