07bb0f4954328704bb8847f62faa402d2603ff48
Laxman Giri <laxmanvatij@gmail.com>
2025-02-08 21:40:03 +0100

    implemented resetpassword api

--- client/src/auth/ResetPassword.tsx
+++ client/src/auth/ResetPassword.tsx
@@ -1,37 +1,46 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { useUserStore } from "@/store/useUserStore"
 import { Loader2, LockKeyholeIcon } from "lucide-react"
-import { ChangeEvent, useState } from "react"
-import { Link } from "react-router"
+import { ChangeEvent, FormEvent, useState } from "react"
+import { Link, useParams } from "react-router"
 
 const ResetPassword=()=>{
     const [newpassword,setnewpassword]=useState<string>("")
-    const submitHandler=()=>{
-        
+    const {token}=useParams()
+    const {loading,resetPassword}=useUserStore()
+    const submitHandler=async(e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        try {
+          await resetPassword(newpassword,token)
+        } catch (error) {
+          console.log(error);
+          
+        }
     }
-    const loading=false
+   
     return (
         <div>
             <div className="flex items-center justify-center min-h-screen  ">
       <form onSubmit={submitHandler} className="border p-4 w-full max-w-md rounded-lg md:p-8">
         <h1 className="text-center text-3xl font-bold ">Forgot Password</h1>
         <h1 className="text-center text-base my-4">
-          Enter your email address to reset your password
+          Enter your  your new password
         </h1>
         <div className="relative ">
           <Input
           value={newpassword}
             onChange={(e:ChangeEvent<HTMLInputElement>)=>setnewpassword(e.target.value)}
-            type="email"
+            type="newpassword"
             className="pl-10 focus-visible:ring-1"
-            placeholder="Enter your email "
+            placeholder="Enter your new password "
           ></Input>
           <LockKeyholeIcon className="absolute inset-2 text-gray-500 pointer-events-none"/>
         </div>
         {loading?<Button className="w-full bg-orange-500 hover:bg-orange-300 my-5 h-10">
           <Loader2 className="animate-spin"/>
         </Button>:<Button className="w-full bg-orange-500 hover:bg-orange-300 my-5 h-10">
-          Send Reset Link
+          Reset Password
         </Button>}
         
         <div className="text-center">
