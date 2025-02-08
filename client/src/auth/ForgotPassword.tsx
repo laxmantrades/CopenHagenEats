0e0a277f9b1818dfa10b1b8f2b15f71ed922ff29
Laxman Giri <laxmanvatij@gmail.com>
2025-02-08 21:39:41 +0100

    implemented forgotpassword api

--- client/src/auth/ForgotPassword.tsx
+++ client/src/auth/ForgotPassword.tsx
@@ -1,32 +1,39 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useUserStore } from "@/store/useUserStore";
 import { Loader2, Mail } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Link } from "react-router";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState<string>("");
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const { loading,forgotPassword } = useUserStore();
 
-    const [email,setEmail]=useState<string>("")
-    const onInputChange=(e:ChangeEvent<HTMLInputElement>)=>{
-        setEmail(e.target.value)
-    }
 
-    const submitHandler=(e:FormEvent)=>{
-            e.preventDefault()
-    }
-    const loading=false
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    forgotPassword(email)
+    
+  };
 
+  
 
   return (
     <div className="flex items-center justify-center min-h-screen  ">
-      <form onSubmit={submitHandler} className="border p-4 w-full max-w-md rounded-lg md:p-8">
+      <form
+        onSubmit={submitHandler}
+        className="border p-4 w-full max-w-md rounded-lg md:p-8"
+      >
         <h1 className="text-center text-3xl font-bold ">Forgot Password</h1>
         <h1 className="text-center text-base my-4">
           Enter your email address to reset your password
         </h1>
         <div className="relative ">
           <Input
-          value={email}
+            value={email}
             onChange={onInputChange}
             type="email"
             className="pl-10 focus-visible:ring-1"
@@ -34,15 +41,21 @@ const ForgotPassword = () => {
           ></Input>
           <Mail className="absolute inset-2 text-gray-500 pointer-events-none"></Mail>
         </div>
-        {loading?<Button className="w-full bg-orange-500 hover:bg-orange-300 my-5 h-10">
-          <Loader2 className="animate-spin"/>
-        </Button>:<Button className="w-full bg-orange-500 hover:bg-orange-300 my-5 h-10">
-          Send Reset Link
-        </Button>}
-        
+        {loading ? (
+          <Button className="w-full bg-orange-500 hover:bg-orange-300 my-5 h-10">
+            <Loader2 className="animate-spin" />
+          </Button>
+        ) : (
+          <Button className="w-full bg-orange-500 hover:bg-orange-300 my-5 h-10">
+            Send Reset Link
+          </Button>
+        )}
+
         <div className="text-center">
-            Back to{" "}
-            <Link to="/login" className="text-blue-500">Login</Link>
+          Back to{" "}
+          <Link to="/login" className="text-blue-500">
+            Login
+          </Link>
         </div>
       </form>
     </div>
