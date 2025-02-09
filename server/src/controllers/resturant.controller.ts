c9d8b102113126c69018463a8a4711ab2dabb28c
Laxman Giri <laxmanvatij@gmail.com>
2025-02-09 22:13:50 +0100

    populated menus && formatted document

--- server/src/controllers/resturant.controller.ts
+++ server/src/controllers/resturant.controller.ts
@@ -46,7 +46,9 @@ export const createResturant: RequestHandler = async (req, res, next) => {
 
 export const getResturant: RequestHandler = async (req, res, next) => {
   try {
-    const resturant = await Resturant.findOne({ user: req.id });
+    const resturant = await Resturant.findOne({ user: req.id }).populate(
+      "menu"
+    );
     if (!resturant) {
       res.status(404).json({
         success: false,
@@ -141,58 +143,57 @@ export const updateOrderStatus: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
-export const searchResturant:RequestHandler=async(req,res,next)=>{
+export const searchResturant: RequestHandler = async (req, res, next) => {
   try {
-    const searchText=req.params.searchText||""
-    const searchQuery=req.query.searchQuery as string||""
-    const selectedCuisines=(req.query.selectedCuisines as string ||"").split(",").filter((cuisine)=>cuisine)
-    let query:any={}
+    const searchText = req.params.searchText || "";
+    const searchQuery = (req.query.searchQuery as string) || "";
+    const selectedCuisines = ((req.query.selectedCuisines as string) || "")
+      .split(",")
+      .filter((cuisine) => cuisine);
+    let query: any = {};
     //basic search based on searchText,name,city,country
-    if(searchText){
-      query.$or=[
-        {resturantName:{$regex:searchText,$options:"i"}},
-        {city:{$regex:searchText,$options:"i"}},
-        {country:{$regex:searchText,$option:"i"}}
-      ]
+    if (searchText) {
+      query.$or = [
+        { resturantName: { $regex: searchText, $options: "i" } },
+        { city: { $regex: searchText, $options: "i" } },
+        { country: { $regex: searchText, $options: "i" } },
+      ];
     }
-    if(searchQuery){
-      query.$or=[
-        {resturantName:{$regex:searchQuery,$options:"i"}},
-        {cuisines:{$regex:searchQuery,$options:"i"}}
-      ]
+    if (searchQuery) {
+      query.$or = [
+        { resturantName: { $regex: searchQuery, $options: "i" } },
+        { cuisine: { $regex: searchQuery, $options: "i" } },
+      ];
     }
     //console.log(query);
-    if(selectedCuisines.length>0){
-      query.cuisines={$in:selectedCuisines}
+    if (selectedCuisines.length > 0) {
+      query.cuisine = { $in: selectedCuisines };
     }
-    const resturant=await Resturant.find(query)
-     res.status(200).json({
-      success:true,
-      data:resturant
-    })
-    
-
-
+    const resturant = await Resturant.find(query);
+    res.status(200).json({
+      success: true,
+      data: resturant,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
-export const getSingleRestaurant:RequestHandler = async (req, res,next) => {
+};
+export const getSingleRestaurant: RequestHandler = async (req, res, next) => {
   try {
-      const restaurantId = req.params.id;
-      const restaurant = await Resturant.findById(restaurantId).populate({
-          path:'menus',
-          options:{createdAt:-1}
+    const restaurantId = req.params.id;
+    const restaurant = await Resturant.findById(restaurantId).populate({
+      path: "menus",
+      options: { createdAt: -1 },
+    });
+    if (!restaurant) {
+      res.status(404).json({
+        success: false,
+        message: "Restaurant not found",
       });
-      if(!restaurant){
-          res.status(404).json({
-              success:false,
-              message:"Restaurant not found"
-          })
-          return
-      };
-      res.status(200).json({success:true, restaurant});
+      return;
+    }
+    res.status(200).json({ success: true, restaurant });
   } catch (error) {
-      next(error)
+    next(error);
   }
-}
+};
