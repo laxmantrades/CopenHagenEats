d3ba96b48457912e8bd59d6dcb82bd910d99343f
Laxman Giri <laxmanvatij@gmail.com>
2025-02-09 22:10:16 +0100

    added Resturant type and resturant state type

--- client/src/types/resturantTypes.ts
+++ client/src/types/resturantTypes.ts
@@ -1,7 +1,48 @@
+import Resturant from "@/admin/Resturant";
+
 export type MenuItem={
     _id:string,
     name:string,
-    descrption:string,
+    description:string,
     price:number,
     image:string
 }
+export type Resturant={
+    _id: string;
+    user: string;
+    resturantName: string;
+    city: string;
+    country: string;
+    deliveryTime: number;
+    cuisine: string[];
+    menu: MenuItem[];
+    imageUrl: string;
+
+
+
+
+}
+export type searchedResturant={
+    data:Resturant[]
+}
+
+
+export type ResturantState = {
+    loading: boolean;
+    resturant:Resturant| null;
+    searchedResturant: searchedResturant|null;
+    appliedFilter: [];
+    //signleResturant: null;
+    //resturantOrder: Orders[];
+    
+    createResturant:(formData:FormData)=>Promise<void>;
+    getResturant:()=>Promise<void>;
+    updateResturant:(formData:FormData)=>Promise<void>;
+    searchResturant:(searchText:string,searchQuery:string,selectedCuisines:any)=>Promise<void>;
+    addMenuToResturant:(menu:MenuItem)=>void;
+    updatedMenuResturant:(updatedMenu:MenuItem)=>void;
+    setAppliedFilter:(value:string)=>void;
+    resetAppliedFilter:()=>void
+  
+  };
+  
