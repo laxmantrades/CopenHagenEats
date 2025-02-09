ef3ab3bbc0411339122f79111c2260cca95ffa8c
Laxman Giri <laxmanvatij@gmail.com>
2025-02-09 22:11:32 +0100

    removed filesize

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -3,8 +3,8 @@ import multer from "multer";
 const storage=multer.memoryStorage()
 const upload=multer({
     storage:storage,
-    limits:{
-        fileSize:5*1024*1024
-    }
+    //limits:{
+      //  fileSize:5*1024*1024
+    //}
 })
 export default upload
