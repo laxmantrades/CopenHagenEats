20d0862f4360a728e195a332d0df932edb716ef6
Laxman Giri <laxmanvatij@gmail.com>
2025-02-11 20:47:21 +0100

    implemented order api

--- client/src/components/Success.tsx
+++ client/src/components/Success.tsx
@@ -2,10 +2,22 @@ import { Link } from "react-router";
 import pizza from "../assets/hero_pizza.png"
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useOrderStore } from "@/store/useOrderStore";
+import { useEffect } from "react";
+import { useCartStore } from "@/store/useCartStore";
 
 const Success = () => {
-  const order = [1,2,3];
-  if (order.length === 0)
+  
+  const {getOrderDetails,orders}=useOrderStore()
+  const{clearCart}=useCartStore()
+
+  useEffect(()=>{
+    getOrderDetails()
+    console.log(orders);
+    clearCart()
+
+  },[])
+  if (orders.length === 0)
     return (
       <div className="flex items-center justify-center min-h-screen">
         <h1 className="font-bold text-2xl text-gray-700 dark:text-gray-300">
@@ -21,16 +33,15 @@ const Success = () => {
         </div>
         <div className="mb-6">
             <h2 className="text-lg font-semibold text-gray-700 dark:text-gray-300">Order Summary</h2>
-            {/**Your item display here */}
-            <div className="mb-4">
-                <div className="flex justify-between items-center">
+            {orders?.map((order)=><div key={order._id} className="mb-4">
+                <div  className="flex justify-between items-center">
                     <div className="flex items-center">
-                        <img src={pizza} className="w-14 h-14 rounded-md object-cover"></img>
-                        <h3 className="ml-3 text-gray-800 dark:text-gray-300 font-medium">Pizza</h3>
+                        <img src={order.cartItems[0].image} className="w-14 h-14 rounded-md object-cover"></img>
+                        <h3 className="ml-3 text-gray-800 dark:text-gray-300 font-medium">{order.cartItems[0].name}</h3>
                     </div>
                     <div className="text-right">
                         <div className="text-gray-700 dark:text-gray-300 flex items-center">
-                            <h2>200Dkk</h2>
+                            <h2>{order.cartItems[0].price}Dkk</h2>
                             
                         </div>
 
@@ -39,7 +50,8 @@ const Success = () => {
                 </div>
                 <Separator className="my-4"/>
 
-            </div>
+            </div>)}
+            
         </div>
         <Link to={"/cart"}>
             <Button className="bg-orange-500 hover:bg-orange-500 w-full">Continue Shopping</Button>
