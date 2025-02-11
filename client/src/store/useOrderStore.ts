fcedb0b50e45933e7fea708eca828f7313936b78
Laxman Giri <laxmanvatij@gmail.com>
2025-02-11 20:47:37 +0100

    added useOrdersStore

--- client/src/store/useOrderStore.ts
+++ client/src/store/useOrderStore.ts
@@ -1,7 +1,39 @@
+import { CheckoutSessionRequest, OrderState } from "@/types/orderType";
+import axios from "axios";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-const useOrderStore=create()(persist((set)=>({
+const API_END_POINT = "http://localhost:3000/api/v1/order";
+axios.defaults.withCredentials = true;
+
+export const useOrderStore=create<OrderState>()(persist((set)=>({
+    loading:false,
+    orders:[],
+    createCheckoutSession: async(checkoutSessionRequest: CheckoutSessionRequest) => {
+        try {
+            set({loading:true})
+            const response=await axios.post(`${API_END_POINT}/checkout/create-checkout-session`,checkoutSessionRequest,{headers:{
+                "Content-Type":"application/json"
+            }})
+           
+            window.location.href=response.data.session.url
+
+
+            set({loading:false})
+
+        } catch (error) {
+            set({loading:false})
+        }
+    },
+    getOrderDetails: async() => {
+        try {
+            set({loading:true})
+            const response=await axios.get(`${API_END_POINT}/`)
+            set({loading:false,orders:response.data.orders})
+        } catch (error) {
+            set({loading:false})
+        }
+    }
     
 
 }),{
