4bd63eb9d96eb3e489ddd1fc40d0dd419e85c478
Laxman Giri <laxmanvatij@gmail.com>
2025-04-21 22:07:52 +0200

    added prisma orm

--- server/src/db/connectDb.ts
+++ server/src/db/connectDb.ts
@@ -1,11 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import mongoose from "mongoose";
-const connectDb=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI!)
-        console.log("Database Succefully connected");
-    } catch (error) {
-       console.log(error);
-        
-    }
-}
-export default connectDb
+export const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+});
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    prisma;
+
+    console.log("Database Succefully connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default connectDb;
