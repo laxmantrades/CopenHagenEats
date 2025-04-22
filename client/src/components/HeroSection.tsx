8dfbf6d0de2dd29cb49821b0b4f55872504e78d6
Laxman Giri <laxmanvatij@gmail.com>
2025-04-22 18:04:16 +0200

    added toast if the search is empty

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,33 +1,57 @@
-import { Search } from "lucide-react"
-import { Input } from "./ui/input"
-import { Button } from "./ui/button"
-import hero from "../assets/hero_pizza.png"
-import { Link } from "react-router"
-import { useState } from "react"
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import hero from "../assets/hero_pizza.png";
 
-const HeroSection=()=>{
-    const[searchtext,setSearchText]=useState<string>("")
-    const onChangeHandler=(e:React.ChangeEvent<HTMLInputElement>)=>{
-            setSearchText(e.target.value)
-    }
-    return(
-        <div className="flex items-center justify-center mt-10">
-            <div>
-                <h1 className="font-extrabold">Order Food anytime</h1>
-                <h1 className="font-extrabold">&anywherer</h1>
-                <p className="mt-2">Hey! Our Delicios food is waiting for you, we are always near to you!</p>
-                <div className="relative mt-4 flex space-x-2">
-                    <Input value={searchtext} onChange={onChangeHandler} placeholder="Search " className=" pl-10 shadow-xl"/>
-                    <Search className="absolute inset-1 text-gray-500"/>
-                    <Link to={`/search/${searchtext}`}><Button >Search</Button></Link>
+import { useState } from "react";
+import { toast } from "sonner";
+import { useNavigate } from "react-router";
 
-                </div>
-                <div>
-                    <img className="object-contain w-full max-h-[500px] " src={hero}></img>
-                </div>
-            </div>
-
+const HeroSection = () => {
+  const [searchtext, setSearchText] = useState<string>("");
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+  const navigate = useNavigate();
+  return (
+    <div className="flex items-center justify-center mt-10">
+      <div>
+        <h1 className="font-extrabold">Order Food anytime</h1>
+        <h1 className="font-extrabold">&anywherer</h1>
+        <p className="mt-2">
+          Hey! Our Delicios food is waiting for you, we are always near to you!
+        </p>
+        <div className="relative mt-4 flex space-x-2">
+          <Input
+            value={searchtext}
+            onChange={onChangeHandler}
+            placeholder="Search "
+            className=" pl-10 shadow-xl"
+          />
+          <Search className="absolute inset-1 text-gray-500" />
+       
+            <Button
+              onClick={() => {
+               
+                if (searchtext.length !== 0) {
+                  navigate(`/search/${searchtext}`);
+                  return
+                }
+               toast.error("Please provide search text");
+              }}
+            >
+              Search
+            </Button>
+        
         </div>
-    )
-}
-export default HeroSection
+        <div>
+          <img
+            className="object-contain w-full max-h-[500px] "
+            src={hero}
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HeroSection;
