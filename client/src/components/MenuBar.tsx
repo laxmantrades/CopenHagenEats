c172e249123ab4ff6b7b13af6b886f514b7c08e5
Laxman Giri <laxmanvatij@gmail.com>
2025-04-22 18:04:29 +0200

    removed logs and added classname to image

--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -2,9 +2,10 @@ import { useCartStore } from "@/store/useCartStore";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import { MenuItem } from "@/types/resturantTypes";
+import { toast } from "sonner";
 
 const MenuBar = ({menus}:{menus:MenuItem[]}) => {
-  console.log(menus);
+ 
   const {addToCart}=useCartStore()
   
   return (
@@ -13,11 +14,11 @@ const MenuBar = ({menus}:{menus:MenuItem[]}) => {
         Available Menu
       </h1>
       <div className="grid md:grid-cols-3 space-y-4 md:space-y-0">
-        {menus?.map((menu:MenuItem)=><Card className="md:max-w-xs mx-auto shadow-lg rounded-lg overflow-hidden">
+        {menus?.map((menu:MenuItem)=><Card key={menu._id} className="md:max-w-xs mx-auto shadow-lg rounded-lg overflow-hidden">
           <img
             src={menu.image}
             alt=""
-            className=" object-cover"
+            className=""
           />
           <CardContent className="p-4">
             <h2 className="text-xl font-semibold text-gray-800 dark:text-white">
@@ -30,8 +31,13 @@ const MenuBar = ({menus}:{menus:MenuItem[]}) => {
                 Price: <span className="text-orange-300">{menu.price}Dk</span>
             </h3>
           </CardContent>
-          <CardFooter className="p-4">
-            <Button onClick={()=>addToCart(menu)} className="bg-orange-500 hover:bg-prange-500 w-full">Add to cart</Button>
+          <CardFooter className="p-4 ">
+            <Button onClick={()=>{
+              addToCart(menu)
+              toast.success(`${menu.name} added to cart`)
+            }
+              
+            } className="bg-orange-500 hover:bg-prange-500 w-full">Add to cart</Button>
           </CardFooter>
         </Card>)}
       </div>
