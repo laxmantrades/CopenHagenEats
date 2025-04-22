4284c520156f54c32ce78df3bab4957254fd5d9d
Laxman Giri <laxmanvatij@gmail.com>
2025-04-22 19:56:26 +0200

    fixed on cookie change

--- server/src/utils/generateCookie.ts
+++ server/src/utils/generateCookie.ts
@@ -5,6 +5,11 @@ import { Document } from "mongoose"
 
 export const generateCookie=(res:Response,user:IUserDocument&Document)=>{
     const token=jwt.sign({userId:user._id},process.env.SECRET_KEY!,{expiresIn:"1d"})
-    res.cookie("token",token,{httpOnly:true, maxAge:24*60*60*1000})
+    res.cookie('token', token, {
+        httpOnly: true, // Ensures the cookie is not accessible via JavaScript
+        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production (HTTPS)
+        sameSite: 'none', // Required if you're working with cross-origin requests
+        maxAge: 24 * 60 * 60 * 1000, // Cookie expiration (1 day)
+      });
     //return token
 }
